019d0852ad76d3b544cb16fbbb64aead78ce535c
Jacopo-De-Martino <jaco.drago.tm@gmail.com>
2025-07-09 15:34:16 +0200

    Create ProvaStrategy.java

---
+++ corsoJavaBackend/lezione13_09_07_2025/Strategy/ProvaStrategy.java
@@ -0,0 +1,43 @@
+package corsoJavaBackend.lezione13_09_07_2025.Strategy;
+
+interface Strategy {
+    void execute();
+}
+
+// Strategie Concrete
+class ConcreteStrategyA implements Strategy {
+    public void execute() {
+        System.out.println("Strategia A eseguita.");
+    }
+}
+
+class ConcreteStrategyB implements Strategy {
+    public void execute() {
+        System.out.println("Strategia B eseguita.");
+    }
+}
+
+// Context
+class Context {
+    private Strategy strategy;
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void performTask() {
+        strategy.execute();
+    }
+}
+
+// Uso da parte del Client
+public class ProvaStrategy {
+    public static void main(String[] args) {
+
+        Context context = new Context();
+        context.setStrategy(new ConcreteStrategyA());
+        context.performTask(); // Output: Strategia A eseguita.
+        context.setStrategy(new ConcreteStrategyB());
+        context.performTask(); // Output: Strategia B eseguita.
+    }
+}

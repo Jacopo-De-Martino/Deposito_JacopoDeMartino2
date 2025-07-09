c0bcf282d5e488e8c08f9fce73149ffd3837e859
Jacopo-De-Martino <jaco.drago.tm@gmail.com>
2025-07-09 18:00:39 +0200

    Prova , Esercizio base Facade

---
+++ corsoJavaBackend/lezione13_09_07_2025/EserciziFacade/MainFacadeEsBase.java
@@ -0,0 +1,36 @@
+package corsoJavaBackend.lezione13_09_07_2025.EserciziFacade;
+
+class AccendiLuceA {// Sottosistema A
+    void accendiLuceA() {
+        System.out.println("Luce A accesa");
+    }
+}
+
+class AccendiLuceB {// Sottosistema B
+    void accendiLuceB() {
+        System.out.println("Luce B accesa");
+    }
+}
+
+class Facade { // Macrosistema che comunica con il Client
+    // Wrapper e inizializzazione sotto sistemi
+    private AccendiLuceA interruttoreA = new AccendiLuceA();
+    private AccendiLuceB interruttoreB = new AccendiLuceB();
+
+    public void accendi() {// Metodo che raggruppa i metodi necessari dei sotto sistemi per svolgere ciò
+                           // che interessa a l'utente accendere le luci
+        // Incapsulamento-astrazione
+        System.out.println("Luce accese con successo");
+        interruttoreA.accendiLuceA();
+        interruttoreB.accendiLuceB();
+    }
+}
+
+// Programma molto basilare
+public class MainFacadeEsBase {
+    public static void main(String[] args) {
+        Facade facade = new Facade();// Istanzio un Oggetto Facade
+        facade.accendi(); // Utilizzo il metodo accendi() dell'oggetto Facade --- vedere commenti metodo
+    }
+
+}
---
+++ corsoJavaBackend/lezione13_09_07_2025/Facade/ProvaFacade.java
@@ -0,0 +1,33 @@
+package corsoJavaBackend.lezione13_09_07_2025.Facade;
+
+// Sottosistema
+class SubsystemA {
+    void operationA() {
+        System.out.println("Operazione A del sottosistema A");
+    }
+}
+
+class SubsystemB {
+    void operationB() {
+        System.out.println("Operazione B del sottosistema B");
+    }
+}
+
+// Facade
+class Facade {
+    private SubsystemA a = new SubsystemA();
+    private SubsystemB b = new SubsystemB();
+
+    public void operazioneUnificata() {
+        a.operationA();
+        b.operationB();
+    }
+}
+
+// Client
+public class ProvaFacade {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.operazioneUnificata();
+    }
+}

f15584d9fd3d85ba6f79ef52548eaa91b725eb19
Jacopo-De-Martino <jaco.drago.tm@gmail.com>
2025-07-16 19:29:32 +0200

    test Connessione con JDBC

---
+++ jdbctest/src/main/java/com/example/App.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/sakila"; // sostituire con il nome del DB
+        String user = "root"; // utente del DB
+        String password = "root"; // password del DB
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connessione riuscita");
+        } catch (SQLException e) {
+            System.out.println("Errore nella connessione");
+            e.printStackTrace();
+        }
+    }
+}
